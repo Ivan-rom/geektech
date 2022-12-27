f6e1079427f3d8cb2bd29e6c45eca8994926dd49
Ivan-rom <94216613+Ivan-rom@users.noreply.github.com>
2022-12-27 08:52:58 +0300

    Add 5th lesson

---
+++ 3-month/5-lesson/js/script.js
@@ -0,0 +1,80 @@
+// ================TABS=====================================
+const tabContents = document.querySelectorAll(".tabcontent");
+const tabButtons = document.querySelectorAll(".tabheader__item");
+const tabButtonsParent = document.querySelector(".tabheader__items");
+
+const hideTabContent = () => {
+    tabContents.forEach(tabContent => tabContent.classList.remove("tabcontent_active"));
+    tabButtons.forEach(tabBtn => tabBtn.classList.remove("tabheader__item_active"));
+};
+
+const showTabConent = (index = 0) => {
+    tabContents[index].classList.add("tabcontent_active");
+    tabButtons[index].classList.add("tabheader__item_active");
+}
+
+hideTabContent();
+showTabConent();
+
+tabButtonsParent.addEventListener("click", e => {
+    const target = e.target;
+    if (!target.classList.contains("tabheader__item_active")) {
+        tabButtons.forEach((button, i) => {
+            if (button === target) {
+                hideTabContent();
+                showTabConent(i);
+            }
+        })
+    }
+});
+
+// ==============MODAL=====================================
+const modal = document.querySelector(".modal")
+const modalOpen = document.getElementById("modal-btn");
+const modalClose = document.querySelector(".modal__close");
+
+modalOpen.addEventListener("click", () => modal.classList.add("modal_open"));
+modalClose.addEventListener("click", () => modal.classList.remove("modal_open"));
+
+
+
+// ===============FORMS=====================================
+const forms = document.querySelectorAll("form");
+
+const messageText = {
+    loading: "Загрузка...",
+    success: "Все успешно сохранено!",
+    error: "Что-то пошло не так!",
+}
+
+const formData = form => {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+
+        const messageBlock = document.createElement("div");
+        messageBlock.textContent = messageText.loading;
+        form.append(messageBlock);
+
+        const formData = new FormData(form);
+
+        const request = new XMLHttpRequest();
+        request.open("POST", "./php/server.php");
+        request.send(formData);
+        request.addEventListener("load", () => {
+            if (request.status === 200) {
+                const response = request.response;
+                console.log(response);
+                messageBlock.textContent = messageText.success;
+            } else {
+                messageBlock.textContent = messageText.error;
+            }
+            setInterval(() => {
+                messageBlock.remove();
+            }, 5000);
+        })
+    })
+}
+
+forms.forEach(form => {
+    formData(form);
+});
