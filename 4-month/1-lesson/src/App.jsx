3d9657573d0be296a380b27a1c850b0aab678798
Ivan notebook <rom_1van@icloud.com>
2023-01-14 19:42:22 +0600

    add 1st lesson of 4th month

---
+++ 4-month/1-lesson/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import User from './User'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <User name="Ivan" age={18}/>
+    </>
+  )
+}
+
+export default App
---
+++ 4-month/1-lesson/src/User.jsx
@@ -0,0 +1,7 @@
+function User(props) {
+    return (
+        <h2>{props.name} {props.age} years</h2>
+    )
+}
+
+export default User
