74ba9bd6327c88fda75eb1ee10e227448418d8a8
Ivan PC <rom_1van@icloud.com>
2023-02-01 12:20:00 +0600

    change oading component to class

--- 4-month/DZ5_ivan_20-03/src/components/Loading/Loading.jsx
+++ 4-month/DZ5_ivan_20-03/src/components/Loading/Loading.jsx
@@ -1,5 +1,14 @@
+import React from "react";
 import styles from "./loading.module.css";
 
-export default function Loading() {
-  return <div className={styles.loading}></div>;
+class Loading extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return <div className={styles.loading}></div>;
+  }
 }
+
+export default Loading;
